ca707c4712f71048bf37315dee4b13b134c95872
Marclap <jhona.cb991128@gmail.com>
2021-04-07 20:15:17 -0500

    Avance en la UI del usuario

--- components/Formulario/index.js
+++ components/Formulario/index.js
@@ -1,94 +1,61 @@
 import { useSate } from 'next'
+import Link from 'next/link'
 export default function Formulario() {
     return (
         <>
-            {/* Contenedor */}
-            <div className="container mx-auto">
-                <div className="flex justify-center px-6 my-12">
-                    {/* Row */}
-                    <div className="xl:w-3/4 lg:w-11/12 flex justify-center">
-                        {/* Col */}
-                        {/* <div
-                            className="h-auto bg-primaryColor hidden lg:block lg::w-5/12 bg-cover rounded-l-lg"
-                            style={{
-                                backgroundImage:
-                                    "url('https://source.unsplash.com/Mv9hjnEUHR4/600x800')",
-                            }}
-                        ></div> */}
-                        {/* Col */}
-                        <div className="w-full lg:w-7/12 bg-primaryColor p-5 rounded-lg lg:rounded-l-none">
-                            <h3 className="pt-4 text-2xl text-center">
-                                ¡Crea tu cuenta!
-                            </h3>
-                            <form className="px-8 pt-6 pb-8 mb-4 bg-primaryColor rounded">
-                                <div className="mb-4 md:flex md:justify-between">
-                                    <div className="mb-4 md:mr-2 md:mb-0">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700">
-                                            Nombre
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="firstName"
-                                            type="text"
-                                            placeholder="Nombre"
-                                        />
-                                    </div>
-                                    <div className="md:ml-2">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700">
-                                            Apellidos
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="lastName"
-                                            type="text"
-                                            placeholder="Apellidos"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block mb-2 text-sm font-bold text-gray-700">
-                                        Email
-                                    </label>
-                                    <input
-                                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
-                                        id="email"
-                                        type="emal"
-                                        placeholder="Email"
-                                    />
-                                </div>
-                                <div className="mb-4 md:flex md:justify-between">
-                                    <div className="mb-4 md:mr-2 md:mb-0">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700">
-                                            Contraseña
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="password"
-                                            type="password"
-                                            placeholder="**********"
-                                        />
-                                    </div>
-                                    <div className="md:ml-2">
-                                        <label className="block mb-2 text-sm font-bold text-gray-700">
-                                            Confirme la contraseña
-                                        </label>
-                                        <input
-                                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                            id="c_password"
-                                            type="password"
-                                            placeholder="**********"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="mb-6 text-center">
-                                    <button
-                                        className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                                        type="button"
-                                    >
-                                        Registrar cuenta
-                                    </button>
-                                </div>
-                            </form>
+            <div className="bg-white min-h-screen flex flex-col">
+                <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                    <div className="bg-primaryColor px-6 py-8 rounded shadow-md text-black w-full">
+                        <h1 className="mb-8 text-3xl text-center">
+                            Regístrate
+                        </h1>
+                        <input
+                            type="text"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="firstName"
+                            placeholder="Nombre"
+                        />
+                        <input
+                            type="text"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="lastName"
+                            placeholder="Apellidos"
+                        />
+                        <input
+                            type="number"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="cedula"
+                            placeholder="Cedula"
+                        />
+                        <input
+                            type="email"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="email"
+                            placeholder="Correo"
+                        />
+                        <input
+                            type="password"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="password"
+                            placeholder="Contraseña"
+                        />
+                        <input
+                            type="password"
+                            className="block border border-gray-400 w-full p-3 rounded mb-4"
+                            name="c_password"
+                            placeholder="Repita la contraseña"
+                        />
+                        <button
+                            type="submit"
+                            className="w-full text-center py-3 rounded bg-secundaryRed text-white hover:bg-primaryRed focus:outline-none my-1"
+                        >
+                            Crear cuenta
+                        </button>
+                    </div>
+                    <div className="mt-6">
+                        ¿Ya tienes una cuenta?
+                        <div className="text-blue-500">
+                            <Link href="/login">Inicia sesión</Link>
                         </div>
                     </div>
                 </div>
