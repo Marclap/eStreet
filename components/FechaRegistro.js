12b350d8e343b64c224a8340b55987eb71b0bff9
Daniel <daniel.ramirez.duque@utp.edu.co>
2021-05-05 21:27:24 -0500

    implementación componente fecha en registro usuario (Color fecha)

--- components/FechaRegistro.js
+++ components/FechaRegistro.js
@@ -5,7 +5,7 @@ export default function FechaRegistro() {
                 <label className="label">Fecha de nacimiento</label>
                 <input
                     type="date"
-                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    className="appearance-none block w-full  white text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     id="grid-city"
                     placeholder="Albuquerque"
                 />
